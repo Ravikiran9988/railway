535231e1938d9d697ddd766918b421a46d122068
Ravikiran9988 <vk8896992@gmail.com>
2025-04-13 23:08:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'http://localhost:5173',
   'https://radiant-phi-ten.vercel.app'
+  'https://radiant-phi-ten.vercel.app/register'
 ];
 
 // ✅ CORS Middleware
